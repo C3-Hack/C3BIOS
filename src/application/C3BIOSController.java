417ef6d011408a5aa131f156b2efcb7d2b8ea37f
coop <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2020-01-28 17:55:49 +0900

    fxmlのバージョンのところ書き換えた

--- src/application/C3BIOSController.java
+++ src/application/C3BIOSController.java
@@ -7,18 +7,20 @@ import javafx.scene.control.Label;
 public class C3BIOSController {
 
 	@FXML
-	private Label label_time;
+	private Label label_timer;
 
 	// 入室ボタンクリック時
 	@FXML
 	protected void onInButtonClick(ActionEvent event) {
 		Main.getInstance().setPage("InPage.fxml");
+		label_timer.setText("入室ボタン押した");
 	}
 
 	// 退室ボタンクリック時
 	@FXML
 	protected void onOutButtonClick(ActionEvent event) {
 		Main.getInstance().setPage("OutPage.fxml");
+		label_timer.setText("退室ボタン押した");
 	}
 
 	// 履歴ボタンクリック時
@@ -31,7 +33,7 @@ public class C3BIOSController {
 	@FXML
 	protected void onReturnButtonClick(ActionEvent event) {
 		Main.getInstance().setPage("TopPage.fxml");
+		label_timer.setText("トップに戻るボタン押した");
 	}
-	
-	
+
 }
