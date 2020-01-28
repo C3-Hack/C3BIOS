ebb694a88bb83b2717f45f16af29bb0d090772f6
naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2020-01-29 00:30:18 +0900

    Controllerから時計関連の記述を削除．代わりにMainに記述したが，ぬるぽが発生する．

--- src/application/C3BIOSController.java
+++ src/application/C3BIOSController.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.time.LocalDateTime;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -15,16 +13,12 @@ public class C3BIOSController {
 	@FXML
 	void onInButtonClick(ActionEvent event) {
 		Main.getInstance().setPage("InPage.fxml");
-		System.out.println("debug1 "+label_time.getText()); // setText前．label_text, 表示共に "1/21 12:34"
-		label_time.setText(LocalDateTime.now().toString()); // 現在時刻を表示する
-		System.out.println("debug2 "+label_time.getText()); // setText後．label_text は変わっているが，表示は "1/21 12:34" のまま
 	}
 
 	// 退室ボタンクリック時
 	@FXML
 	void onOutButtonClick(ActionEvent event) {
 		Main.getInstance().setPage("OutPage.fxml");
-		label_time.setText("退室");
 	}
 
 	// 履歴ボタンクリック時
@@ -37,7 +31,6 @@ public class C3BIOSController {
 	@FXML
 	void onReturnButtonClick(ActionEvent event) {
 		Main.getInstance().setPage("TopPage.fxml");
-		label_time.setText("トップ");
 	}
 
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,17 @@
 package application;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 public class Main extends Application {
@@ -14,12 +21,14 @@ public class Main extends Application {
 	private Stage stage;
 	private final int WINDOW_WIDTH = 600;  // ウインドウの幅
 	private final int WINDOW_HEIGHT = 600; // ウインドウの高さ
+	@FXML private Label label_time;
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			main_class = this;
 			stage = primaryStage;
+			runClock();
 
 			// ページを表示する
 			setPage("TopPage.fxml"); // TopPageを表示する
@@ -36,7 +45,7 @@ public class Main extends Application {
 
 	// 表示するページを設定する
 	// 引数fxml : fxmlのファイル名
-	public void setPage(String fxml) {
+	void setPage(String fxml) {
 		try {
 			root = (BorderPane)FXMLLoader.load(getClass().getResource(fxml));
 			stage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
@@ -48,5 +57,21 @@ public class Main extends Application {
 	// Controller から Main クラスを呼び出す際に使う
 	public static Main getInstance() {
 		return main_class;
+	}
+
+	// 時計を動かす
+	void runClock() {
+		// 1000ミリ秒ごとに更新する
+		Timeline timer = new Timeline(new KeyFrame(Duration.millis(1000), new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("debug1");
+				//label_time.setText(LocalDateTime.now().toString()); // ここで例外（ぬるぽ）が投げられているようだ．おそらく label_time が原因．
+				System.out.println("debug2");
+			}
+		}));
+
+		timer.setCycleCount(Timeline.INDEFINITE); // 無限に繰り返す
+		timer.play();
 	}
 }
