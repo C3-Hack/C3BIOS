4bc556dea1fa0203da50efff4ac387493a9e9977
naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2020-02-02 23:32:37 +0900

    時計を実装した

--- src/application/C3BIOSController.java
+++ src/application/C3BIOSController.java
@@ -1,13 +1,25 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 public class C3BIOSController {
 
-	@FXML
-	private Label label_time;
+	@FXML Label label_clock;
+
+	// 最初に呼ばれる
+	public void initialize() {
+		label_clock.setText(new SimpleDateFormat("MM/dd HH:mm:ss").format(new Date()));
+		runClock(label_clock);
+	}
 
 	// 入室ボタンクリック時
 	@FXML
@@ -31,6 +43,20 @@ public class C3BIOSController {
 	@FXML
 	void onReturnButtonClick(ActionEvent event) {
 		Main.getInstance().setPage("TopPage.fxml");
+	}
+
+	// 時計を動かす
+	void runClock(Label label) {
+		// 100ミリ秒ごとに更新する
+		Timeline clock = new Timeline(new KeyFrame(Duration.millis(100), new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				label.setText(new SimpleDateFormat("MM/dd HH:mm:ss").format(new Date()));
+			}
+		}));
+
+		clock.setCycleCount(Timeline.INDEFINITE); // 無限に繰り返す
+		clock.play();
 	}
 
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,37 +1,33 @@
 package application;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 
 public class Main extends Application {
 
 	public static Main main_class;
-	private BorderPane root;
 	private Stage stage;
+	private Scene topPage;
+	private Scene inPage;
+	private Scene outPage;
 	private final int WINDOW_WIDTH = 600;  // ウインドウの幅
 	private final int WINDOW_HEIGHT = 600; // ウインドウの高さ
-	@FXML private Label label_time;
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			main_class = this;
 			stage = primaryStage;
-			runClock();
+			topPage = new Scene(FXMLLoader.load(getClass().getResource("TopPage.fxml")), WINDOW_WIDTH, WINDOW_HEIGHT);
+			inPage = new Scene(FXMLLoader.load(getClass().getResource("InPage.fxml")), WINDOW_WIDTH, WINDOW_HEIGHT);
+			outPage = new Scene(FXMLLoader.load(getClass().getResource("OutPage.fxml")), WINDOW_WIDTH, WINDOW_HEIGHT);
 
 			// ページを表示する
 			setPage("TopPage.fxml"); // TopPageを表示する
+			stage.setTitle("C3BIOS"); // タイトル設定
 			stage.setResizable(false); // ウインドウサイズ変更不可に設定
 			stage.show();
 		} catch(Exception e) {
@@ -39,39 +35,43 @@ public class Main extends Application {
 		}
 	}
 
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+
 	// 表示するページを設定する
-	// 引数fxml : fxmlのファイル名
 	void setPage(String fxml) {
 		try {
-			root = (BorderPane)FXMLLoader.load(getClass().getResource(fxml));
-			stage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
+			switch(fxml) {
+			case "TopPage.fxml":
+				stage.setScene(topPage);
+				break;
+
+			case "InPage.fxml":
+				stage.setScene(inPage);
+				break;
+
+			case "OutPage.fxml":
+				stage.setScene(outPage);
+				break;
+
+			default:
+				break;
+			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 
 	// Controller から Main クラスを呼び出す際に使う
 	public static Main getInstance() {
 		return main_class;
 	}
 
-	// 時計を動かす
-	void runClock() {
-		// 1000ミリ秒ごとに更新する
-		Timeline timer = new Timeline(new KeyFrame(Duration.millis(1000), new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("debug1");
-				//label_time.setText(LocalDateTime.now().toString()); // ここで例外（ぬるぽ）が投げられているようだ．おそらく label_time が原因．
-				System.out.println("debug2");
-			}
-		}));
 
-		timer.setCycleCount(Timeline.INDEFINITE); // 無限に繰り返す
-		timer.play();
-	}
+
+
 }
