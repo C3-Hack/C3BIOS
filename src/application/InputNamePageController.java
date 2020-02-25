009bf1c875dbefb7f722e7cfe67c123cb8364aab
naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2020-02-25 18:59:43 +0900

    add: 名前を入力してcsvに書き込む機能を追加
    InputNamePageController 上に書いた通り．

---
+++ src/application/InputNamePageController.java
@@ -0,0 +1,19 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class InputNamePageController {
+
+	@FXML private TextField textField_name;
+	private Utilities utilities = new Utilities();
+
+	// 名前入力画面の入室ボタンをクリックしたとき
+	@FXML
+	void onInButtonInErrorWindowClick(ActionEvent event) {
+		System.out.println(textField_name.getText());
+		utilities.writeCSV("InOutTime.csv", "00 00 00 00 00 00 00 00", "182C1000", utilities.getTime("yyMMdd"), utilities.getTime("HH:mm"));
+	}
+
+}
