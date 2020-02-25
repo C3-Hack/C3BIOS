297e88392e6ec484512278cd617d0cd2c5952a25
naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2020-02-25 19:34:58 +0900

    add: 名前入力で不正な入力を受け付けないようにした
    InputNamePageController.java#19 コンマを含む文字列と空文字列は受け付けないようにした

--- src/application/InputNamePageController.java
+++ src/application/InputNamePageController.java
@@ -2,18 +2,32 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class InputNamePageController {
 
 	@FXML private TextField textField_name;
+	@FXML private Label label_warning;
 	private Utilities utilities = new Utilities();
 
 	// 名前入力画面の入室ボタンをクリックしたとき
 	@FXML
 	void onInButtonInErrorWindowClick(ActionEvent event) {
-		System.out.println(textField_name.getText());
-		utilities.writeCSV("InOutTime.csv", "00 00 00 00 00 00 00 00", "182C1000", textField_name.getText(), utilities.getTime("yyMMdd"), utilities.getTime("HH:mm"));
+		String username = textField_name.getText();
+
+		if(username.equals("")) {
+			// 名前が入力されなかった場合もcsvには書き込まない
+			label_warning.setText("名前を入力してください");
+		} else if(username.contains(",")) {
+			// 名前にコンマが含まれていたらcsvには書き込まない
+			label_warning.setText("名前に \",\" は使えません．");
+		} else {
+			// それ以外はcsvに書き込み
+			utilities.writeCSV("InOutTime.csv", "00 00 00 00 00 00 00 00", "182C1000", username, utilities.getTime("yyMMdd"), utilities.getTime("HH:mm"));
+			label_warning.setText("");
+		}
+
 	}
 
 }
